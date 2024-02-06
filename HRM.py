8cb58d9dcf858d09d079a33167bee43c34e83263
mohammed aabith <mohammedaabith87@gmail.com>
2024-02-06 18:51:53 +0530

    Initial commit

---
+++ HRM.py
@@ -0,0 +1,31 @@
+class Employee:
+    def __init__(self,Emp_Id, Emp_Name, Emp_Dep, Emp_Contact, Emp_Appoiment, Emp_Status):
+
+        self.Emp_Id = Emp_Id
+        self.Emp_Name = Emp_Name
+        self.Emp_Dep = Emp_Dep
+        self.Emp_Contact = Emp_Contact
+        self.Emp_Appoiment = Emp_Appoiment
+        self.Emp_Status = Emp_Status
+
+    def add_Employee(self,Emp_Id, Emp_Name, Emp_Dep, Emp_Contact, Emp_Appoiment, Emp_Status):
+
+        pass
+
+    def view_emp_info(self):
+
+        pass
+
+    def update_emp_info(self):
+
+        pass
+
+    def leave_request(self):
+
+        pass
+
+    def view_company_policy(self):
+
+        pass
+
+    def
